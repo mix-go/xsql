22d3da899df3d77d53861991635e8c9ad7261afa
soen <970852318@qq.com>
2023-05-05 14:50:08 +0800

    feat:增加WhereObj结构体，用来组装where条件语句

--- query.go
+++ query.go
@@ -1,6 +1,9 @@
 package xsql
 
 import (
+	"fmt"
+	"reflect"
+	"strings"
 	"time"
 )
 
@@ -31,4 +34,32 @@ func (t *query) Fetch(query string, args []interface{}, opts *Options) (*Fetcher
 		Options: opts,
 	}
 	return f, err
+}
+
+// WhereObj
+// @Description: where条件语句组装器
+type WhereObj struct {
+	Str string
+}
+
+func WhereMaker() *WhereObj {
+	return &WhereObj{
+		Str: "",
+	}
+}
+func (this *WhereObj) FieldCondi(field, exp string, value any) *WhereObj {
+	t := reflect.TypeOf(value)
+	if t.Name() == "int" || t.Name() == "int32" || t.Name() == "int64" || t.Name() == "int16" {
+		this.Str += fmt.Sprintf("%s %s %v AND ", field, exp, value)
+	} else {
+		this.Str += fmt.Sprintf("%s %s '%v' AND ", field, exp, value)
+	}
+	return this
+}
+
+func (this *WhereObj) ToStr() string {
+	this.Str = strings.TrimSuffix(this.Str, " AND ")
+	s := "WHERE " + this.Str
+	this.Str = ""
+	return s
 }
